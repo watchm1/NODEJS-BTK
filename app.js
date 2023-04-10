cc3de90a50b4a3b60e75214e760e9ce16845e525
root <root@DESKTOP-B5CNH78.localdomain>
2023-04-10 03:10:59 +0300

    router and body parser

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <html>
+            <head>
+                <title>Add a new product</title>
+            </head>
+            <body>
+                <form action="/product" method="POST">
+                    <input type="text" name"productName">
+                    <input type="submit" value="save product">
+                </form> 
+            </body>
+        </html>
+    `)
+});
 
-app.use((req, res, next) => {
-    console.log("middleware 1 çalıştırıldı");
-    next();
-})
-app.use((req,res, next) => {
-    console.log("middleware 2 çalıştırıldı.");
-    next();
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use("/",(req,res, next) => {
+    console.log("main url çalıştı");
+    res.send();
 })
 app.listen(3000, () => {
     console.log("listening on 3000 port");
