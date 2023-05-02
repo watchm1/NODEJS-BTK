bbf42b8ee9f66d31aff8cdbaa494b07a7f289ea4
watchm1 <m.ali.software.dev@gmail.com>
2023-05-02 16:02:33 +0300

    updated crud operations for categories and pages done

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+
+
+
 const sequelize = require('./utilities/database');
 const User = require('./models/user');
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -65,3 +65,54 @@ exports.postDeleteProduct = (req, res, next) => {
         res.redirect('/admin/products?action=delete');
     }).catch((error) => {console.log(error)});
 }
+exports.getAddCategory = (req, res, next) => {
+   res.render('admin/add-category', {
+       title: 'Add new Category',
+       path:'admin/add-category'
+   });
+}
+exports.postAddCategory = (req, res, next) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    Category.create({
+        name: name,
+        description: description
+    }).then((result) => {
+        res.redirect('/admin/categories?action=create');
+    }).catch((error) => {});
+}
+exports.getCategories = (req, res,next) => {
+    Category.findAll().then((categories) => {
+        res.render('admin/categories', {
+            title: 'New product',
+            categories: categories,
+            path: '/admin/categories',
+            action: req.query.action,
+        })
+    }).catch(error => console.log(error));
+}
+exports.getEditCategory = (req, res, next) => {
+    const categoryid = req.params.categoryid;
+    Category.findByPk(categoryid).then((category) => {
+        res.render('admin/edit-category', {
+            title: 'Edit category',
+            path: '/admin/category',
+            category: category
+        })
+    }).catch(err => console.log(err));
+}
+exports.postEditCategory = (req, res, next) => {
+    const categoryid = req.body.id;
+    const name = req.body.name;
+    const description = req.body.description;
+    Category.update({name: name, description: description}, {where: {id: categoryid}}).then(() => {
+        res.redirect('/admin/categories?action=edit')
+    }).catch((error) => console.log(error));
+
+}
+exports.postDeleteCategory = (req, res, next) => {
+    const categoryId = req.body.categoryid;
+    Category.destroy({where: {id: categoryId}}).then(() => {
+        res.redirect('/admin/categories?action=delete');
+    }).catch((error) => {console.log(error)});
+}
